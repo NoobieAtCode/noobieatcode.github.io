69a14cf0790b954cd3b73e47a3f0238fc4eca12f
NoobieAtCode <136034949+NoobieAtCode@users.noreply.github.com>
2025-03-01 14:57:12 -0800

    add lights-out from AIC

---
+++ games/lights-out.js
@@ -0,0 +1,35 @@
+$(document).ready(()=>{
+    $("body").append("<div class='winbox'>You Won</div>");
+    $("body").append("<div class='resetbutton'>Reset</div>");
+    $("div.winbox").hide();
+    $("table tr:first-child td").eq(2).addClass("light-on");
+    $("table tr:nth-child(2) td").eq(1).addClass("light-on");
+    $("table tr:nth-child(3) td").eq(4).addClass("light-on");
+    $("table tr:nth-child(4) td").eq(2).addClass("light-on");
+    $("table tr:nth-child(5) td").eq(3).addClass("light-on");
+    $("table").on("click", "tr td", (event) => {
+        $(event.target).toggleClass("light-on");
+        let localindex = $(event.target).index();
+        $(event.target.previousSibling).toggleClass('light-on');
+        $(event.target.nextSibling).toggleClass('light-on');
+        $(event.target).parent().prev().children().eq(localindex).toggleClass('light-on');
+        $(event.target).parent().next().children().eq(localindex).toggleClass('light-on');
+        if ($("table tr td.light-on").length == 0) {
+            $("div.winbox").toggle()
+            $("div.winbox").on("click", (event) => {
+                $("div.winbox").toggle();
+            })
+        }
+    });
+    $("div.resetbutton").on("click", () => {
+        $("table tr td").each((index) => {
+            $("table tr td").eq(index)[0].className = ""
+        })
+        $("table tr:first-child td").eq(2).addClass("light-on");
+        $("table tr:nth-child(2) td").eq(1).addClass("light-on");
+        $("table tr:nth-child(3) td").eq(4).addClass("light-on");
+        $("table tr:nth-child(4) td").eq(2).addClass("light-on");
+        $("table tr:nth-child(5) td").eq(3).addClass("light-on");
+        $("div.winbox").hide();
+    })
+});
